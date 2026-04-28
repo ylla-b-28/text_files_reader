1230e2781c347c2484a6f6d7c444b2fa0d8b5ca5
ylla-b-28 <ylla.briza28@gmail.com>
2026-04-29 04:41:07 +0800

    I added the variables.

--- text_file_reader/student_gwa_reader/student_gwa_reader.py
+++ text_file_reader/student_gwa_reader/student_gwa_reader.py
@@ -1,7 +1,18 @@
 #open file
+student_file = open("students.txt", "r")
 
-#student name
+best_gwa = -1.0
+best_student_name = ""
 
-#gwa
+#split student name and gwa
+student_name =
+student_gwa =
 
 #check gwa if higher
+if student_gwa > best_gwa:
+    best_gwa = student_gwa
+    best_student_name = student_name
+
+student_file.close()
+
+print("The top student is", best_student_name, "with a GWA of", best_gwa)
