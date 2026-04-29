8e3f49b168f55a7a654ce8a751947b79a08b435f
ylla-b-28 <ylla.briza28@gmail.com>
2026-04-29 15:53:06 +0800

    I added the variables and basic structure.

--- text_file_reader/multiple_line_context_file/multiple_line_context_file.py
+++ text_file_reader/multiple_line_context_file/multiple_line_context_file.py
@@ -1,7 +1,11 @@
 #write a file
-
+f = open("mylife.txt", "w")
 #enter line
-
+more = "y"
+while more == "y":
+    line = input("Enter line: ")
+    f.write(line + "\n")
 #question
-
+    more = input("Are there more lines y/n? ")
 #close
+f.close()
