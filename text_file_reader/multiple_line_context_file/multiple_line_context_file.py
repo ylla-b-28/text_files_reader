0a635bbb96ca4daf7d0aca026a6deee782d00208
ylla-b-28 <ylla.briza28@gmail.com>
2026-04-29 15:59:20 +0800

    I changed the variable names and added with statement.

--- text_file_reader/multiple_line_context_file/multiple_line_context_file.py
+++ text_file_reader/multiple_line_context_file/multiple_line_context_file.py
@@ -1,11 +1,15 @@
-#write a file
-f = open("mylife.txt", "w")
-#enter line
-more = "y"
-while more == "y":
-    line = input("Enter line: ")
-    f.write(line + "\n")
-#question
-    more = input("Are there more lines y/n? ")
-#close
-f.close()
+def record_life_lines():
+    filename = "mylife.txt"
+
+    with open(filename, "a") as diary_file:
+        active = True
+
+        while active:
+            user_input = input("Enter line: ")
+            diary_file.write(user_input + "\n")
+
+            choice = input("Are there more lines y/n? ").lower()
+            if choice == "n":
+                active = False
+
+record_life_lines()
